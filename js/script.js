0f195a064100111d5a627bc8c00768ec4b0e5198
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2020-09-22 17:34:26 -0600

    Javascript inicial

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 // Lista de tareas
 //
 
+const formulario = document.getElementById('new-task-form');
+
+formulario.addEventListener('submit', (event) => {
+  // Se cancela el comportamiento default del formulario.
+  event.preventDefault();
+  // Texto introducido por el usuario.
+  console.log(formulario.elements[0].value);
+})
